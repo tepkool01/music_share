34c3f9b09af5dc2211b3b9feb269367df3684678
Michael Young <mvy1221@gmail.com>
2021-11-27 00:08:07 -0800

    adding back icons

---
+++ client/src/components/icons/IconForwards.vue
@@ -0,0 +1,12 @@
+<template>
+  <path d="M8 13.5v-5l-5 5v-11l5 5v-5l5.5 5.5z"></path>
+</template>
+
+<script>
+export default {
+  name: "IconForwards"
+}
+</script>
+
+<style scoped>
+</style>
---
+++ client/src/components/icons/IconPause.vue
@@ -0,0 +1,12 @@
+<template>
+  <path d="M2 2h5v12h-5zM9 2h5v12h-5z"></path>
+</template>
+
+<script>
+export default {
+  name: "IconPause"
+}
+</script>
+
+<style scoped>
+</style>
