89191515b526c8432f3a98e971b4aa56eec058e1
Michael Young <mvy1221@gmail.com>
2021-11-28 13:21:05 -0800

    working socket connection without CORS issue

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-// import VueSocketIO from 'vue-socket.io';
+import VueSocketIO from 'vue-3-socket.io'
 
 const VueApp = createApp(App)
 VueApp.use(router)
-// VueApp.use(new VueSocketIO({
-// 	debug: true,
-// 	connection: 'http://localhost:3333',
-// }));
+
+VueApp.use(
+	new VueSocketIO({
+		debug: true, // debug debugging, production recommended to close
+		connection: "http://localhost:5000"
+	})
+);
+
 VueApp.mount('#app')
--- server/musicServer.js
+++ server/musicServer.js
@@ -1,13 +1,23 @@
-const os = require('os');
-const http = require('http');
-const { Server } = require("socket.io");
+// const os = require('os');
+// import os from 'os'
 
+const http = require('http');
+// const { Server } = require("socket.io");
+// import http from 'http'
+// import { Server } from 'socket.io'
 // Create express and port
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 5000;
 
 // Web Socket / Long polling with socket.io
 const server = http.createServer();
-const io = new Server(server);
+const io = require("socket.io")(server, {
+	allowEIO3: true,
+	cors: {
+		origin: "http://localhost:8080",
+		methods: ["GET", "POST"],
+		credentials: true
+	}
+});
 
 // Socket handlers [imports]-- files that are used as 'listeners' for data from the client (front-end javascript)
 // these socket handlers respond back to the client application in real time, as the client is listening
@@ -25,30 +35,33 @@ io.on('connection', (socket) => {
 	registerKingHandlers(io, socket);
 });
 
+// Start server
+server.listen(port, () => {
+	// let privateIP = getPrivateIP();
+	// console.log('Private Network IP: http://' + privateIP + ':' + port)
+	// console.log('listening on *:' + port);
+	console.log('Server started at http://localhost:' + port);
+});
+
+
 // Static Routes
 // app.use(express.static(__dirname + '/public'));
 // app.get('/', function(req, res) {
 // 	res.sendFile(path.join(__dirname, '/index.html'));
 // });
 
-// Start server
-server.listen(port, () => {
-	let privateIP = getPrivateIP();
-	console.log('listening on *:' + port);
-	console.log('Private Network IP: http://' + privateIP + ':' + port)
-	console.log('Server started at http://localhost:' + port);
-});
 
-function getPrivateIP() {
-	// Get local network IP address
-	let networkInterfaces = os.networkInterfaces();
-	let privateIP = '';
-	if (networkInterfaces.hasOwnProperty('Wi-Fi')) {
-		privateIP = networkInterfaces['Wi-Fi'][4]['address'];
-	} else if (networkInterfaces.hasOwnProperty('Ethernet')) {
-		privateIP = networkInterfaces['Ethernet'][4]['address'];
-	} else {
-		console.log(networkInterfaces);
-	}
-	return privateIP;
-}
+
+// function getPrivateIP() {
+// 	// Get local network IP address
+// 	let networkInterfaces = os.networkInterfaces();
+// 	let privateIP = '';
+// 	if (networkInterfaces.hasOwnProperty('Wi-Fi')) {
+// 		privateIP = networkInterfaces['Wi-Fi'][4]['address'];
+// 	} else if (networkInterfaces.hasOwnProperty('Ethernet')) {
+// 		privateIP = networkInterfaces['Ethernet'][4]['address'];
+// 	} else {
+// 		console.log(networkInterfaces);
+// 	}
+// 	return privateIP;
+// }
