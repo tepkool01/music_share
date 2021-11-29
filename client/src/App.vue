8a5d151b49d8336e5baa0620427de2cdcdf48f25
Zoe McDaniel <zmcdaniel@gmail.com>
2021-11-28 17:46:44 -0800

    resolving merge conflict, clean up

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,8 @@
 <template>
-  <Header />
   <router-view></router-view>
-  <Footer />
 </template>
 
 <script>
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
-
-export default {
-  components: {
-    Header,
-    Footer
-  }
-}
 </script>
 
 <style>
